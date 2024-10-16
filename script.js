ccb9582efc4f3365d86481d3ba5bce7e611f4b50
3pm-on-github <pookiemon2019@gmail.com>
2024-10-16 19:29:09 +0200

    added addmod function

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ var states = {}
 // --- CODE ---
 
 // hey now that i think about it its actually super difficult-
+
+const script = document.createElement('script');
+script.src = 'presets.js';
+document.head.appendChild(script);
+
+function addMod(modname) {
+    const script = document.createElement('script');
+    script.src = 'mods/'+ modname +'/otherFile.js';
+    document.head.appendChild(script);
+}
